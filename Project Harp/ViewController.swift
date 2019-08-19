f98e8c01dcbb85dc855b50be20f257e79643c68c
Devante McFarlane <devante@webbintegrated.com>
2019-08-19 19:04:13 -0400

    Initial Commit

---
+++ Project Harp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Harp
+//
+//  Created by Devante McFarlane on 8/19/19.
+//  Copyright © 2019 Devante McFarlane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project HarpTests/Project_HarpTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_HarpTests.swift
+//  Project HarpTests
+//
+//  Created by Devante McFarlane on 8/19/19.
+//  Copyright © 2019 Devante McFarlane. All rights reserved.
+//
+
+import XCTest
+@testable import Project_Harp
+
+class Project_HarpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
